a860b345c1d5b2b2141812c3b9e88ee8958c1eeb
vrena4567 <v.rena4567@gmail.com>
2023-06-13 10:59:53 +0200

    add hungarian names

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Month thisMonth = Month.JUNE;
-        System.out.println("This months is: " + thisMonth);
+        System.out.println("This months is: " + thisMonth.getHungarianName());
         for (Month month : Month.values() ) {
-            System.out.println(month);
+            System.out.println(month.getHungarianName());
         }
 
         Season thisSeason = Season.SUMMER;
--- src/Month.java
+++ src/Month.java
@@ -1,14 +1,25 @@
 public enum Month {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTERMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER;
+    JANUARY("Január"),
+    FEBRUARY("Február"),
+    MARCH("Március"),
+    APRIL("Április"),
+    MAY("Május"),
+    JUNE("Június"),
+    JULY("Július"),
+    AUGUST("Augusztus"),
+    SEPTERMBER("Szeptember"),
+    OCTOBER("Október"),
+    NOVEMBER("November"),
+    DECEMBER("December");
+
+    private String hungarianName;
+
+    Month(String hungarianName) {
+        this.hungarianName = hungarianName;
+    }
+    public String getHungarianName() {
+        return this.hungarianName;
+    }
+
+
 }
