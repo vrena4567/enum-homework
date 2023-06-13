1b2ecf914e238624856ece507557f2cb67d29e82
vrena4567 <v.rena4567@gmail.com>
2023-06-13 10:34:13 +0200

    add month

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Month thisMonth = Month.JUNE;
+        System.out.println("This months is: " + thisMonth);
+        for (Month month : Month.values() ) {
+            System.out.println(month);
+        }
     }
 }
