8b07df9ab65486132769cbe09eaf47297f511999
vrena4567 <v.rena4567@gmail.com>
2023-06-13 10:43:25 +0200

    add season

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         for (Month month : Month.values() ) {
             System.out.println(month);
         }
+
+        Season thisSeason = Season.SUMMER;
+        System.out.println("We are in the " + thisSeason + " season.");
+        for (Season season: Season.values()) {
+            System.out.println(season);
+        }
     }
 }
